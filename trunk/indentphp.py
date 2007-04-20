fae0491889e17f4656fbb5b9579683a4dad845e9
mbertheau <mbertheau@a0d91f9e-8d2e-0410-bb36-2ba89cdadaa9>
2007-04-20 16:33:38 +0000

    * Initial version

---
+++ trunk/indentphp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# indentphp is (c) 2007 by Markus Bertheau
+# indentphp is distributed under the GNU General Public License Version 2
+
+# configuration
+
+strIndent = '    '
+
+# end configuration
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
