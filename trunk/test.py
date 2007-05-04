f4a7fef2d13543c9a2d6e329d50bb772c8febb23
mbertheau <mbertheau@a0d91f9e-8d2e-0410-bb36-2ba89cdadaa9>
2007-05-04 19:18:03 +0000

    Unit testing + doesn't destroy html only files :)

--- trunk/test.py
+++ trunk/test.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 
-import indentphp
+from indentphp import indentfile
 import unittest
+from glob import glob
+from commands import getoutput, mkarg
+from os import unlink
+
+class TestIndentPHP(unittest.TestCase):
+    def testall(self):
+        files = glob('tests/???-*.php')
+        files.sort()
+        for infilename in files:
+            outfilename = 'tests/out%s' % infilename[5:]
+            expectedfilename = 'tests/expected%s' % infilename[5:]
+            indentfile(infilename, outfilename)
+            output = getoutput('LC_ALL=C diff -u %s %s' % (mkarg(outfilename),  
+                                                 mkarg(expectedfilename)))
+            unlink(outfilename)
+            self.assertEqual(output, '', "\n" + output)
+
 
 if __name__ == '__main__':
     unittest.main()
---
+++ trunk/tests/001-onlyhtml.php
@@ -0,0 +1,5 @@
+<html><head><title>foo</title></head>
+<body>
+	<p>Indentphp indents your PHP source code.</p>
+</body>
+</html>
